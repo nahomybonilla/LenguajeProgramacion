5216c28c32e65a052529055b4167cf3844d2f8ad
nahomybonilla <157558333+nahomybonilla@users.noreply.github.com>
2024-03-09 09:50:02 -0600

    Add files via upload

---
+++ S8/database.py
@@ -0,0 +1,50 @@
+import mysql.connector
+import os
+
+class MYSQLDB:
+    def __init__(self,host,user,password,database):
+        self.host= host
+        self.user= user
+        self.pw= password
+        self.db= database
+        self.connection= None
+        
+    def connect (self):
+        try:
+            if (self.connect == None):
+                self.connection = mysql.connector.connect (
+                    host = self.host,
+                    user = self.user,
+                    password = self.pw,
+                    database = self.db
+                )
+                 
+                print ("congratulations")
+        except mysql.connector.Error as error:
+            print ("Error mientras se estaba conectando {}".format (error))
+            
+    def disconnect(self):
+        try:
+            if self.connection:
+                   self.connection.close()
+                   self.connection = None
+        except mysql.connector.Error as error:
+            print ("Error")
+            
+            
+    def execute_query (self, query, params=None):
+         try:
+             cursor = self.connection.cursor()
+             cursor.execute(query,params)  
+             result = cursor.fetchall ()
+             return result
+         except mysql.connector.Error as error:
+             print(f"Error: (err)")
+                 
+            
+            
+db = MYSQLDB ("localhost", "root", "","tstlp")
+print ("Conectando")
+            
+db.connect()
+categorias = db.execute_query("select * from categorias")
